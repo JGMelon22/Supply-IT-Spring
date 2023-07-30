cb03aed960842ffad99701f90d541863f3d3439f
JGMelon22 <jgvasconcellos22@gmail.com>
2023-07-30 12:32:08 -0300

    first commit

---
+++ src/main/java/com/dpi/itsupply/ItSupplyApiApplication.java
@@ -0,0 +1,13 @@
+package com.dpi.itsupply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSupplyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItSupplyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dpi/itsupply/ItSupplyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpi.itsupply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSupplyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
