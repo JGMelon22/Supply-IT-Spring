7d08b34c40fdfc73533b5cb16e8d9b2310a71627
JGMelon22 <jgvasconcellos22@gmail.com>
2023-07-30 16:18:49 -0300

    Desenvolvendo a DTO de ErroQuiq e adicionando o pacote Validations

---
+++ src/main/java/com/dpi/itsupply/dto/ErroQuiqRecordDto.java
@@ -0,0 +1,20 @@
+package com.dpi.itsupply.dto;
+
+import com.dpi.itsupply.model.enums.Mes;
+import com.dpi.itsupply.model.enums.Plataforma;
+import com.dpi.itsupply.model.enums.StatusAnalise;
+import com.dpi.itsupply.model.enums.StatusErro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record ErroQuiqRecordDto(@NotBlank LocalDate dataPadronizada, @NotNull Mes mes, @NotNull Integer ano,
+                                @NotBlank LocalDateTime dataPedido, @NotBlank LocalDateTime dataErro,
+                                @NotNull Plataforma plataforma, @NotNull Short empresa, @NotBlank String numeroPedido,
+                                @NotNull Integer numeroPdv, @NotNull Byte codigoStatus, @NotBlank String erro,
+                                @NotBlank String motivoErro, @NotBlank String responsavelErro,
+                                @NotNull StatusErro statusErro, @NotBlank String observacao,
+                                @NotBlank String responsavelAnalise, @NotNull StatusAnalise statusAnalise) {
+}
